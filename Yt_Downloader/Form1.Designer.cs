4d21a10dbf1da31600fdbb1597d611c285a535d7
Qufel <97023524+Qufel@users.noreply.github.com>
2022-09-20 22:39:11 +0200

    initial commit

---
+++ Yt_Downloader/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace Yt_Downloader
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.gb1 = new System.Windows.Forms.GroupBox();
+            this.browse_directory_btn = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.url = new System.Windows.Forms.ColumnHeader();
+            this.title = new System.Windows.Forms.ColumnHeader();
+            this.author = new System.Windows.Forms.ColumnHeader();
+            this.extension = new System.Windows.Forms.ColumnHeader();
+            this.length = new System.Windows.Forms.ColumnHeader();
+            this.progress = new System.Windows.Forms.ColumnHeader();
+            this.status = new System.Windows.Forms.ColumnHeader();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.gb1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // gb1
+            // 
+            this.gb1.Controls.Add(this.browse_directory_btn);
+            this.gb1.Controls.Add(this.textBox2);
+            this.gb1.Controls.Add(this.label2);
+            this.gb1.Controls.Add(this.textBox1);
+            this.gb1.Controls.Add(this.label1);
+            this.gb1.Location = new System.Drawing.Point(12, 12);
+            this.gb1.Name = "gb1";
+            this.gb1.Size = new System.Drawing.Size(860, 150);
+            this.gb1.TabIndex = 0;
+            this.gb1.TabStop = false;
+            this.gb1.Text = "URL and Directory";
+            // 
+            // browse_directory_btn
+            // 
+            this.browse_directory_btn.Location = new System.Drawing.Point(779, 48);
+            this.browse_directory_btn.Name = "browse_directory_btn";
+            this.browse_directory_btn.Size = new System.Drawing.Size(75, 23);
+            this.browse_directory_btn.TabIndex = 4;
+            this.browse_directory_btn.Text = "Browse";
+            this.browse_directory_btn.UseVisualStyleBackColor = true;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(76, 48);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(697, 23);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 51);
+            this.label2.Margin = new System.Windows.Forms.Padding(3);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(48, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Save to:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(76, 19);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(778, 23);
+            this.textBox1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Margin = new System.Windows.Forms.Padding(3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Video URL:";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button3);
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.listView1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 168);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(860, 381);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Download List";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 352);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(109, 23);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "Remove Selected";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.url,
+            this.title,
+            this.author,
+            this.extension,
+            this.length,
+            this.progress,
+            this.status});
+            this.listView1.GridLines = true;
+            this.listView1.Location = new System.Drawing.Point(6, 22);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(848, 324);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // url
+            // 
+            this.url.Text = "URL";
+            this.url.Width = 250;
+            // 
+            // title
+            // 
+            this.title.Text = "Title";
+            this.title.Width = 150;
+            // 
+            // author
+            // 
+            this.author.Text = "Author";
+            this.author.Width = 100;
+            // 
+            // extension
+            // 
+            this.extension.Text = "Extension";
+            this.extension.Width = 80;
+            // 
+            // length
+            // 
+            this.length.Text = "Length";
+            this.length.Width = 80;
+            // 
+            // progress
+            // 
+            this.progress.Text = "Progress";
+            this.progress.Width = 80;
+            // 
+            // status
+            // 
+            this.status.Text = "Status";
+            this.status.Width = 100;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(619, 352);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 23);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "Download Selected";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(749, 352);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(105, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "Download All";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 561);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.gb1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.gb1.ResumeLayout(false);
+            this.gb1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private GroupBox gb1;
+        private GroupBox groupBox2;
+        private ListView listView1;
+        private ColumnHeader columnHeader1;
+        private ColumnHeader columnHeader2;
+        private ColumnHeader columnHeader3;
+        private ColumnHeader columnHeader4;
+        private ColumnHeader columnHeader5;
+        private ColumnHeader columnHeader6;
+        private ColumnHeader columnHeader7;
+        private ColumnHeader url;
+        private ColumnHeader title;
+        private ColumnHeader author;
+        private ColumnHeader extension;
+        private ColumnHeader length;
+        private ColumnHeader progress;
+        private ColumnHeader status;
+        private TextBox textBox1;
+        private Label label1;
+        private TextBox textBox2;
+        private Label label2;
+        private Button browse_directory_btn;
+        private Button button1;
+        private Button button3;
+        private Button button2;
+    }
+}
